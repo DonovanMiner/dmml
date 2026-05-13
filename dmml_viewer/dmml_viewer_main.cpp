e06651cd2e5bc344d789e55c7f95d3a9c9db308b
DonovanMiner <donovanminer@gmail.com>
2026-05-13 18:08:43 -0400

    -

--- dmml_viewer/dmml_viewer_main.cpp
+++ dmml_viewer/dmml_viewer_main.cpp
@@ -90,27 +90,27 @@ int main() {
 
 
 	unsigned int vertArray_m, vertBuffer_m, idxBuffer_m = 0;
-	/*glGenVertexArrays(1, &vertArray_m);
-	glBindVertexArray(vertArray_m);
+	//glGenVertexArrays(1, &vertArray_m);
+	//glBindVertexArray(vertArray_m);
 
-	glGenBuffers(1, &vertBuffer_m);
-	glBindBuffer(GL_ARRAY_BUFFER, vertBuffer_m);
+	//glGenBuffers(1, &vertBuffer_m);
+	//glBindBuffer(GL_ARRAY_BUFFER, vertBuffer_m);
 
-	float vertices[3 * 3] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
-	};
+	//float vertices[3 * 3] = {
+	//	-0.5f, -0.5f, 0.0f,
+	//	0.5f, -0.5f, 0.0f,
+	//	0.0f, 0.5f, 0.0f
+	//};
 
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
 
-	glGenBuffers(1, &idxBuffer_m);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBuffer_m);
+	//glGenBuffers(1, &idxBuffer_m);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, idxBuffer_m);
 
-	unsigned int indices[3] = { 0, 1, 2 };
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
+	//unsigned int indices[3] = { 0, 1, 2 };
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
 
